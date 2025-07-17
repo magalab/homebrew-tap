bb2dc978488ce0e825d740cc74de4d5084a36161
lurenyang <lurenyang@outlook.com>
2025-07-17 14:35:49 +0800

    fix: trim sha256 prefix

--- Casks/idev.rb
+++ Casks/idev.rb
@@ -2,12 +2,12 @@ cask "idev" do
   version "0.0.2"
 
   on_arm do
-    sha256 "sha256:52c739084c38437eeaa4c9a35630a9dff70d295a4b5f7a892878222b8822354a"
+    sha256 "52c739084c38437eeaa4c9a35630a9dff70d295a4b5f7a892878222b8822354a"
     url "https://github.com/magalab/idev/releases/download/v#{version}/idev-v#{version}-arm64.dmg"
   end
 
   on_intel do
-    sha256 "sha256:9a84e6bbb1106dca672366b2ec79b33379add5140b94011d852bb4bb3f4304a0"
+    sha256 "9a84e6bbb1106dca672366b2ec79b33379add5140b94011d852bb4bb3f4304a0"
     url "https://github.com/magalab/idev/releases/download/v#{version}/idev-v#{version}-x86_64.dmg"
   end
   name "idev"
