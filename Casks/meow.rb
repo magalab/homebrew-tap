5eb43e6bad68ab15c6c463243445381ba1b06200
lurenyang <lurenyang@outlook.com>
2026-04-22 10:05:44 +0800

    fix: remove sha256 prefix from meow cask

--- Casks/meow.rb
+++ Casks/meow.rb
@@ -1,6 +1,6 @@
 cask "meow" do
   version "0.0.2"
-  sha256 "sha256:6a768c5b7020de6cd94f61d1389614e75ca41e04a42a4629c9a8001bb9b09e36"
+  sha256 "6a768c5b7020de6cd94f61d1389614e75ca41e04a42a4629c9a8001bb9b09e36"
 
   url "https://github.com/magalab/meow/releases/download/v#{version}/Meow_#{version}_arm64.dmg"
   name "Meow"
