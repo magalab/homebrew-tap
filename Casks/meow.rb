8bb3b98ce58e4d87e86e3f89f3192b8a8c5d248a
lurenyang <lurenyang@outlook.com>
2026-05-31 22:29:47 +0800

    update: bump meow version to 0.0.5 and update sha256

--- Casks/meow.rb
+++ Casks/meow.rb
@@ -1,6 +1,6 @@
 cask "meow" do
-  version "0.0.4"
-  sha256 "1083463cfcb3bf5ee1cf83c14b033ff2d01413ca64dc83e6a2843a80b0ef6ee0"
+  version "0.0.5"
+  sha256 "067c6c0cfe4115976bb3e5878f00dc3c8d34d65a1ac1420a0a9fb8dd543f71f5"
 
   url "https://github.com/magalab/meow/releases/download/v#{version}/Meow_#{version}_arm64.dmg"
   name "Meow"
